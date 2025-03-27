da8289a7fedbf65c06fa9245a05e4410aa881f8b
dev <dev@DESKTOP-G66RQCV>
2025-03-27 12:29:23 +0500

    Added Domail Layer

--- src/CryptoNova.Domain/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace CryptoNova.Domain;
-
-public class Class1
-{
-
-}
---
+++ src/CryptoNova.Domain/Entities/Key.cs
@@ -0,0 +1,11 @@
+﻿using CryptoNova.Domain.Commons;
+using CryptoNova.Domain.Enums;
+
+namespace CryptoNova.Domain.Entities;
+
+public class Key : Auditable
+{
+    public long UserId { get; set; }
+    public AlgorithmType AlgorithmType { get; set; }
+    public string KeyValue {  get; set; }
+}
