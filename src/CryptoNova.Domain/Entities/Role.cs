8a639a8c70207ec7e502b1b5eb5d26de0404dfe5
dev <dev@DESKTOP-G66RQCV>
2025-03-28 12:03:54 +0500

    Added Role, Permission, RolePermission entities . Update User entity

---
+++ src/CryptoNova.Domain/Entities/Role.cs
@@ -0,0 +1,10 @@
+﻿using CryptoNova.Domain.Commons;
+
+namespace CryptoNova.Domain.Entities;
+
+public sealed class Role : Auditable
+{
+
+    public string Name { get; set; }
+    public IEnumerable<RolePermission> RolePermissions { get; set; }
+}
---
+++ src/CryptoNova.Domain/Entities/RolePermission.cs
@@ -0,0 +1,11 @@
+﻿using CryptoNova.Domain.Commons;
+
+namespace CryptoNova.Domain.Entities;
+
+public sealed class RolePermission : Auditable
+{
+    public long RoleId { get; set; }
+    public Role Role { get; set; }
+    public long PermissionId { get; set; }
+    public Permission Permission { get; set; }
+}
